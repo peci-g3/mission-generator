edf9d7433a88fcf3c20b3594d70d5b471bb3c552
João Tomás <jtsimoes@ua.pt>
2022-04-02 16:37:30 +0100

    Improve overall commands with Groovy syntax Added 3 more available commands: assign, arm and home

--- script.js
+++ script.js
@@ -2,36 +2,50 @@ const containers = document.querySelectorAll('.container')
 
 function init() {
 	document.querySelector('#commands').innerHTML = `
-    <div class="draggable take-off" draggable="true">
-      take off
-    </div>
+		<div class="draggable assign" draggable="true">
+			assign
+		</div>
 
-    <div class="draggable move" draggable="true">
-      move
-      <input type="number" name="m" id="m" value="5" min="0" required>
-      meters
-      <select name="p" id="p" required>
-        <option value="forward">forward</option>
-        <option value="backwards">backwards</option>
-        <option value="left">left</option>
-        <option value="right">right</option>
-        <option value="up">up</option>
-        <option value="down">down</option>
-      </select>
-    </div>
+		<div class="draggable arm" draggable="true">
+			arm
+		</div>
 
-    <div class="draggable rotate" draggable="true">
-      rotate
-      <input type="number" name="d" id="d" value="45" min="0" required>
-      degrees
-      <select name="r" id="r" required>
-        <option value="clockwise">clockwise</option>
-        <option value="counterclockwise">counterclockwise</option>
-      </select>
-    </div>
+		<div class="draggable take-off" draggable="true">
+			take off
+		</div>
 
-    <div class="draggable land" draggable="true">land</div>
-  `
+		<div class="draggable move" draggable="true">
+			move
+			<input type="number" name="m" id="m" value="5" min="0" required>
+			meters
+			<select name="p" id="p" required>
+				<option value="forward">forward</option>
+				<option value="backwards">backwards</option>
+				<option value="left">left</option>
+				<option value="right">right</option>
+				<option value="up">up</option>
+				<option value="down">down</option>
+			</select>
+		</div>
+
+		<div class="draggable rotate" draggable="true">
+			rotate
+			<input type="number" name="d" id="d" value="45" min="0" required>
+			degrees
+			<select name="r" id="r" required>
+				<option value="clockwise">clockwise</option>
+				<option value="counterclockwise">counterclockwise</option>
+			</select>
+		</div>
+
+		<div class="draggable land" draggable="true">
+			land
+		</div>
+
+		<div class="draggable home" draggable="true">
+			home
+		</div>
+	`
 
 	draggables = document.querySelectorAll('.draggable')
 
@@ -92,13 +106,28 @@ document.querySelector('#generate').addEventListener('submit', function (e) {
 		return false
 	}
 
-	if (!hasClass(divChildren[4], 'take-off')) {
-		alert('Could not generate mission JSON, because the first command of the timeline must be "take off"!')
+	if (divChildren.length < 7) {
+		alert('Could not generate mission JSON, because timeline does not have enough commands!	\nMust have one "assign", one "arm" and one "take off" command obligatorily.')
 		return false
 	}
 
-	if (!hasClass(divChildren[divChildren.length - 1], 'land')) {
-		alert('Could not generate mission JSON, because the last command of the timeline must be "land"!')
+	if (!hasClass(divChildren[4], 'assign')) {
+		alert('Could not generate mission JSON, because the first command of the timeline must be "assign"!')
+		return false
+	}
+
+	if (!hasClass(divChildren[5], 'arm')) {
+		alert('Could not generate mission JSON, because the second command of the timeline must be "arm"!')
+		return false
+	}
+
+	if (!hasClass(divChildren[6], 'take-off')) {
+		alert('Could not generate mission JSON, because the third command of the timeline must be "take off"!')
+		return false
+	}
+
+	if (!hasClass(divChildren[divChildren.length - 1], 'land') && !hasClass(divChildren[divChildren.length - 1], 'home')) {
+		alert('Could not generate mission JSON, because the last command of the timeline must be "land" or "home"!')
 		return false
 	}
 
@@ -108,22 +137,35 @@ document.querySelector('#generate').addEventListener('submit', function (e) {
 		console.log('id: ' + i + ' | type: ' + divChildren[i].className)
 
 		switch (divChildren[i].className) {
+			case 'draggable assign':
+				text = 'drone = assign any'
+				break
+
+			case 'draggable arm':
+				text = 'arm drone'
+				break
+
 			case 'draggable take-off':
-				text = 'TAKEOFF'
+				// TODO: Access values from inputs to customize commands
+				text = 'takeoff drone, 5.m'
 				break
 
 			case 'draggable move':
 				// TODO: Access values from inputs to customize commands
-				text = 'MOVE ...'
+				text = 'move drone, forward: 20.m, speed: 5.m/s'
 				break
 
 			case 'draggable rotate':
 				// TODO: Access values from inputs to customize commands
-				text = 'ROTATE ...'
+				text = 'turn drone, 90.deg'
 				break
 
 			case 'draggable land':
-				text = 'LAND'
+				text = 'land drone'
+				break
+
+			case 'draggable home':
+				text = 'home drone'
 				break
 
 			default:
