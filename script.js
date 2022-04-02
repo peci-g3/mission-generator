f89e60e6350db9a41f4c53fd266042bca0b865f9
João Tomás <jtsimoes@ua.pt>
2022-04-02 15:07:15 +0100

    Started implementation of JSON mission generator

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const containers = document.querySelectorAll('.container')
 
 function init() {
-	document.querySelector("#commands").innerHTML = `
+	document.querySelector('#commands').innerHTML = `
     <div class="draggable take-off" draggable="true">
       take off
     </div>
@@ -75,3 +75,73 @@ function getDragAfterElement(container, y) {
 		}
 	}, { offset: Number.NEGATIVE_INFINITY }).element
 }
+
+function hasClass(element, className) {
+	return ('' + element.className + '').indexOf('' + className + '') > -1
+}
+
+document.querySelector('#generate').addEventListener('submit', function (e) {
+	e.preventDefault()
+
+	var result = ''
+	var div = document.getElementById('timeline')
+	var divChildren = div.childNodes
+
+	if (divChildren.length < 5) {
+		alert('Could not generate mission JSON, because timeline is empty!')
+		return false
+	}
+
+	if (!hasClass(divChildren[4], 'take-off')) {
+		alert('Could not generate mission JSON, because the first command of the timeline must be "take off"!')
+		return false
+	}
+
+	if (!hasClass(divChildren[divChildren.length - 1], 'land')) {
+		alert('Could not generate mission JSON, because the last command of the timeline must be "land"!')
+		return false
+	}
+
+	for (var i = 4; i < divChildren.length; i++) {
+
+		// TODO: Debug
+		console.log('id: ' + i + ' | type: ' + divChildren[i].className)
+
+		switch (divChildren[i].className) {
+			case 'draggable take-off':
+				text = 'TAKEOFF'
+				break
+
+			case 'draggable move':
+				// TODO: Access values from inputs to customize commands
+				text = 'MOVE ...'
+				break
+
+			case 'draggable rotate':
+				// TODO: Access values from inputs to customize commands
+				text = 'ROTATE ...'
+				break
+
+			case 'draggable land':
+				text = 'LAND'
+				break
+
+			default:
+				text = 'UNKNOWN'
+		}
+
+		result += text + '\n'
+	}
+
+	// TODO: Debug
+	console.log('FINAL JSON:\n' + result)
+
+	if (result.indexOf('UNKNOWN') > -1) {
+		alert('Could not generate mission JSON, because an unknown command was provided on the timeline!')
+		return false
+	}
+
+	// TODO: Export JSON or print it on a new tab/window
+	alert('Mission JSON generated successfully!')
+	return true
+})
